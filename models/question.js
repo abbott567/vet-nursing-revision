089183eac472ce0bb7782af77f766c60b64d8f12
Craig Abbott <craigabbott.digitaldwp@gmail.com>
2017-02-01 21:20:11 +0000

    Reverted createdAt schema

--- models/question.js
+++ models/question.js
@@ -4,15 +4,13 @@ const answerSchema = new mongoose.Schema({
   answer: String
 });
 
-const createdAt = new mongoose.Schema({
-  type: Date,
-  default: Date.now
-});
-
 const questionSchema = new mongoose.Schema({
   question: String,
   answers: [answerSchema],
-  createdAt
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 module.exports.Question = questionSchema;
