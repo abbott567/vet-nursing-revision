0700617540fb610548bacc3880501f87bd74c608
Craig Abbott <craigabbott.digitaldwp@gmail.com>
2017-02-01 21:17:25 +0000

    Updated schema

--- models/question.js
+++ models/question.js
@@ -4,10 +4,15 @@ const answerSchema = new mongoose.Schema({
   answer: String
 });
 
+const createdAt = new mongoose.Schema({
+  type: Date,
+  default: Date.now
+});
+
 const questionSchema = new mongoose.Schema({
   question: String,
   answers: [answerSchema],
-  createdAt: {type: Date, default: Date.now}
+  createdAt
 });
 
 module.exports.Question = questionSchema;
