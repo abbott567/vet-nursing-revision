821c5c00631d86d109c8c35c0401d77383443e50
Craig Abbott <craigabbott.digitaldwp@gmail.com>
2017-02-01 23:17:08 +0000

    Added shuffle function for answers

--- views/common/functions.js
+++ views/common/functions.js
@@ -1,5 +1,13 @@
 module.exports = {
   randomNum: (min, max) => {
     return Math.floor((Math.random() * (max - min + 1)) + min);
+  },
+
+  shuffle(array) {
+    for (let i = array.length; i; i--) {
+      const n = Math.floor(Math.random() * i);
+      [array[i - 1], array[n]] = [array[n], array[i - 1]];
+    }
+    return array;
   }
 };
--- views/index/functions.js
+++ views/index/functions.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const {randomNum} = require('../common/functions');
+const {shuffle} = require('../common/functions');
 
 const Question = mongoose.model('Question');
 
@@ -7,7 +8,9 @@ module.exports = {
   getQuestion: answeredQuestions => {
     return Question.find({_id: {$nin: answeredQuestions}})
     .then(results => {
-      return results[randomNum(0, randomNum.length - 1)];
+      const result = results[randomNum(0, randomNum.length - 1)];
+      result.answers = shuffle(result.answers);
+      return result;
     });
   }
 };
