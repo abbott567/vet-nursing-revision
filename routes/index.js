3058173038c827db4e651102ab176d088ff1126d
Craig Abbott <craigabbott.digitaldwp@gmail.com>
2017-02-01 20:12:45 +0000

    Converted to xo standards

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const router = new express.Router();
+
+router.get('/', (req, res) => {
+  res.render('index', {title: 'Express'});
 });
 
 module.exports = router;
