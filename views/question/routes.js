b870b007015d1eae422fe767ccc73cef7e1d9f6a
Craig Abbott <craigabbott.digitaldwp@gmail.com>
2017-02-02 15:41:21 +0000

    Added correct and incorrect tallys

--- views/question/routes.js
+++ views/question/routes.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const {getQuestion} = require('./functions');
 
+const Question = mongoose.model('Question');
 const router = new express.Router();
 
 router.get('/:catId/question', (req, res, next) => {
@@ -18,11 +20,26 @@ router.get('/:catId/question', (req, res, next) => {
   });
 });
 
-router.post('/:catId/question', (req, res) => {
+router.post('/:catId/question', (req, res, next) => {
   const answeredQuestions = req.cookies.answeredQuestions || [];
   answeredQuestions.push(req.body.id);
   res.cookie('answeredQuestions', answeredQuestions);
-  res.redirect(`/cat/${req.params.catId}/question`);
+  Question.findOne({_id: req.body.id})
+  .then(q => {
+    if (req.body.answer === q.correct) {
+      const correct = req.cookies.correct || [];
+      correct.push(q.id);
+      res.cookie('correct', correct);
+    } else {
+      const incorrect = req.cookies.incorrect || [];
+      incorrect.push(q.id);
+      res.cookie('incorrect', incorrect);
+    }
+    res.redirect(`/cat/${req.params.catId}/question`);
+  })
+  .catch(err => {
+    next(err);
+  });
 });
 
 module.exports = router;
